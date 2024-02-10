9b1d94d46c7e076fccaa48bc0cab2bbd6ad43115
MasterBro Network <60338675+MasterBroNetwork@users.noreply.github.com>
2024-02-10 11:48:09 +0000

    Update raylib.h

--- src/raylib.h
+++ src/raylib.h
@@ -321,6 +321,7 @@ typedef struct Camera3D {
     Vector3 position;       // Camera position
     Vector3 target;         // Camera target it looks-at
     Vector3 up;             // Camera up vector (rotation over its axis)
+    float cameraMoveSpeed;  // Camera move speed: 0.09f by default
     float fovy;             // Camera field-of-view aperture in Y (degrees) in perspective, used as near plane width in orthographic
     int projection;         // Camera projection: CAMERA_PERSPECTIVE or CAMERA_ORTHOGRAPHIC
 } Camera3D;
